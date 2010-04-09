1bab61621704d0c4b2d79ece5ca1055f5b7295b9
Zach Gardner <locriani@gmail.com>
2010-04-09 18:42:29 -0500

    Uniqueness test, articles (also, I fail at committing)

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,13 +1,27 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "Article Name is Unique" do
+  test "Article Title is Unique" do
     
     articles = []
     same_title = "STRING"
     
     articles[0] = Factory.build(:article, :title => same_title)
     articles[1] = Factory.build(:article, :title => same_title)
+    
+    assert articles[0].valid?
+    articles[0].save!
+    
+    assert !(articles[1].valid?)
+  end
+  
+  test "Article Title is not Case Sensitive (for uniqueness)" do
+    
+    articles = []
+    same_title = "STRING"
+    
+    articles[0] = Factory.build(:article, :title => same_title.upcase)
+    articles[1] = Factory.build(:article, :title => same_title.downcase)
     
     assert articles[0].valid?
     articles[0].save!
