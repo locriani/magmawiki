71cb744cb9de0e97df4207c74f3ae036f13ad9e2
Zach Gardner <locriani@gmail.com>
2010-04-09 18:04:44 -0500

    Articles test case

---
+++ test/factories/article.rb
@@ -0,0 +1,3 @@
+Factory.define :article do |article|
+  article.title 'Some article'
+end
--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "" do
+  test "Article Name is Unique" do
+    
+    articles = []
+    same_title = "STRING"
+    
+    articles[0] = Factory.build(:article, :title => same_title)
+    articles[1] = Factory.build(:article, :title => same_title)
+    
+    assert articles[0].valid?
+    articles[0].save!
+    
+    assert !(articles[1].valid?)
   end
 end
