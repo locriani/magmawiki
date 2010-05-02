57b48e8cf0495815821beb13b362653b50021b04
Zach Gardner <locriani@gmail.com>
2010-05-02 08:54:21 -0500

    Remove captcha for now (work is progressing in captcha branch)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(params[:user])
-    if CAPTCHA.verify_recaptcha(:model => @user, :message => (I18n.t 'recaptcha.error')) && @user.save
+    if @user.save
       flash[:notice] = I18n.t 'user.create_message'
       redirect_to root_url
     else
