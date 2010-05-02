14622c18a94441c9397e829be9c50c608fa685eb
epitron <chris@ill-logic.com>
2010-05-02 11:05:22 -0400

    * Annotate models plugin * Annotated models * Extra .gitignore line for RubyMine

--- app/models/talk_forum.rb
+++ app/models/talk_forum.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+# Schema version: 20100428021347
+#
+# Table name: talk_forums
+#
+#  id         :integer         not null, primary key
+#  created_at :datetime        
+#  updated_at :datetime        
+#
+
 class TalkForum < ActiveRecord::Base
 end
