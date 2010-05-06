e6d6e10be6bcd5cc6241e03eb0502d2f9ec6c34f
epitron <chris@ill-logic.com>
2010-05-06 01:09:42 -0400

    Annotated models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+# Schema version: 20100513034408
+#
+# Table name: comments
+#
+#  id         :integer         not null, primary key
+#  user_id    :integer         
+#  topic_id   :integer         
+#  subject    :string(255)     
+#  body       :text            
+#  parent_id  :integer         
+#  left       :integer         
+#  right      :integer         
+#  created_at :datetime        
+#  updated_at :datetime        
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :topic
 
