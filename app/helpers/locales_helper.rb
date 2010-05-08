8a9862e7c407eb050c8f975412c0445a78f28547
Zach Gardner <locriani@gmail.com>
2010-05-07 22:27:56 -0500

    Some random locales / preferences work

---
+++ app/helpers/locales_helper.rb
@@ -0,0 +1,5 @@
+module LocalesHelper
+  def available_locales
+    I18n.available_locales
+  end
+end
--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20100507065544
+#
+# Table name: user_preferences
+#
+#  id         :integer         not null, primary key
+#  preference :string(255)     
+#  value      :string(255)     
+#  user_id    :integer         
+#  created_at :datetime        
+#  updated_at :datetime        
+#
+
 class UserPreference < ActiveRecord::Base
   belongs_to :user
 end
