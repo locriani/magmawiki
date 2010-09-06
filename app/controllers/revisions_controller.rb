bb6a2cfa7508bba82b711aee5f9a3f6970139939
Emalee <rs.of.chrs@gmail.com>
2010-09-06 01:30:20 -0400

    user pref email + rev history/diff stuff

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -23,4 +23,20 @@ class RevisionsController < ApplicationController
       end
     end
   end
+  
+  def diff
+    @article = Article.find_by_slug(params[:id])
+	@revision = @article.revisions.find(params[:revision_1_id])
+	@revision2 = @article.revisions.find(params[:revision_2_id])
+	    @toolbar_locals = { :article	          => @article,
+  						          :article_active		  => true,
+  						          :read_active        => true}
+    if @revision.nil?
+      redirect_to show_history_path(@article)
+    else
+      respond_to do |format|
+        format.html
+      end
+    end	
+  end
 end
