b92c7a094150ca6530cd2f52af231f5830f06de8
Emalee <rs.of.chrs@gmail.com>
2010-09-16 03:26:50 -0700

    some parser function support

--- config/initializers/monkeypatches.rb
+++ config/initializers/monkeypatches.rb
@@ -71,25 +71,3 @@ module Enumerable
 
 end
 
-module WikiCloth
-  class WikiLinkHandler
-    def section_link(section)
-	  "/editsec/#{section}/#{self.params[:pagename]}"
-    end
-
-    def template(template, args = nil)
-      tmpname = template.gsub(/\s/,"_")
-      article = Article.find_by_slug(tmpname.downcase, :include => :current_revision)
-      article.nil? ? nil : article.current_revision.body
-    end
-  end
-end
-
-class WikiParser < WikiCloth::Parser
-
-  url_for do |url|
-    url.gsub(/\s/,"_")
-  end
-
-end
-
