fc3e33a5373aca123e863c85ee0ba942d53b56e8
Zach Gardner <locriani@gmail.com>
2010-10-12 08:46:43 -0500

    Rails 3 will now boot. * Fixed Wikicloth dependency (part of the gemfile) * Removed old 2.3.x new_rails_defaults.rb file

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.0'
 
 gem 'authlogic', :git => "http://github.com/binarylogic/authlogic.git"
+gem 'wikicloth'
 
 # group :development, :test do
 gem 'sqlite3-ruby', :require => 'sqlite3'
--- config/initializers/load_magmawiki_config.rb
+++ config/initializers/load_magmawiki_config.rb
@@ -2,6 +2,3 @@ MAGMAWIKI_CONFIG = YAML::load(File.read(RAILS_ROOT + "/config/magmawiki_config.y
 
 I18n.default_locale = MAGMAWIKI_CONFIG[:locale][:default].to_sym
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-
-CAPTCHA = Captcha::Recaptcha.new(MAGMAWIKI_CONFIG[:captcha])
-
