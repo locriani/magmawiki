f5c6e77d2e5f67a4a2641e23314d0aa0777e8a25
Zach Gardner <locriani@gmail.com>
2010-10-13 06:27:53 -0500

    Remove deprecated call to RAILS_ROOT, use ::Rails.root.to_s instead

--- config/initializers/load_magmawiki_config.rb
+++ config/initializers/load_magmawiki_config.rb
@@ -1,4 +1,4 @@
-MAGMAWIKI_CONFIG = YAML::load(File.read(RAILS_ROOT + "/config/magmawiki_config.yml").untaint)['magmawiki'].with_indifferent_access
+MAGMAWIKI_CONFIG = YAML::load(File.read(::Rails.root.to_s + "/config/magmawiki_config.yml").untaint)['magmawiki'].with_indifferent_access
 
 I18n.default_locale = MAGMAWIKI_CONFIG[:locale][:default].to_sym
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
