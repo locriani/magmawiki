886931af9908a3df8c02e53c764bf86333ea09fe
Emalee <rs.of.chrs@gmail.com>
2010-12-14 02:47:35 -0800

    minor cosmetic improvements

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,4 +1,12 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
   attr_accessible :user_id, :provider, :uid
+  
+  def provider_name  
+    if provider == 'open_id'  
+      "OpenID"  
+    else  
+      provider.titleize  
+    end  
+  end
 end
