144ff249b2fcaa93fda1b8eee67578ddb666720d
Emalee <rs.of.chrs@gmail.com>
2010-12-14 02:47:35 -0800

    minor cosmetic improvements

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
+require 'openid/store/filesystem'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   provider :facebook, '183517451659073', 'c4994a19ad6f467a360e1e3509e6676d'
+  provider :open_id, OpenID::Store::Filesystem.new('/tmp')  
 end
