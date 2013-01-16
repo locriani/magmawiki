25c373ad60deb9e90e107ece7aa8f2d413758058
Emily Dobervich <rs.of.chrs@gmail.com>
2013-01-15 18:09:00 -0800

    new rails 3.2.11 app w/ rspec and haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magmawiki::Application.config.secret_token = 'b946920ddde0e71de89072d0a79a977acd3c4d00e6a7a8184bf718a6a6afeb8374fa69e2af2fc06f05e3322e35e0f9dac47e07f5d8438e1e4156f78e6935548c'
